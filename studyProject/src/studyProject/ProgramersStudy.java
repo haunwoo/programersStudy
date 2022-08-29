c042746fc934bdf0062c043371e3e10ada558682
jhw <jhw@DESKTOP-GTR8NQ3>
2022-08-29 17:49:18 +0900

    키패드

---
+++ studyProject/src/studyProject/ProgramersStudy.java
@@ -0,0 +1,30 @@
+package studyProject;
+
+public class ProgramersStudy {
+
+	public static void main(String[] args) {
+		
+		int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+		String hands = "right";
+		
+		String result = keypad(numbers, hands);
+		
+		System.out.println(result);
+		
+	}
+
+	public static String keypad(int[] numbers, String hand) {
+		String handKey = "";
+	
+		//내 손이 있는 위치를 체크 
+		//그 위치에서 부터 다음에 눌러야 하는 키패드의 위치를 계산하는 함수를 짜야함
+		//나누거나 빼면 값이 나옴 
+		
+		
+		
+		
+		
+		return handKey;
+	}
+	
+}
